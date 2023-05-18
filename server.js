0e8ae8caaae744a8a20c771d57eefe3eefced14f
OscarPereyra1992 <oscarpereyra.mhs@gmail.com>
2023-05-18 16:02:11 -0300

    cambios generales

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ contactEmail.verify((error) => {
   }
 });
 
-app.post("/contact", (req, res) => {
+app.post("/api/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { Col, Row, Container } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 
-
-
 export const Contact = () => {
   const formInitialDetails = {
     firstName: "",
@@ -42,7 +40,7 @@ export const Contact = () => {
     } else {
       setStatus({
         success: false,
-        message: "Oh no! your messagge is not sending.",
+        message: "Oh no! Tu mensaje no se envió.",
       });
     }
   };
@@ -92,7 +90,7 @@ export const Contact = () => {
                 </Col>
                 <Col>
                   <textarea
-                    row="6"
+                    rows="6"
                     value={formDetails.message}
                     placeholder="Mensaje"
                     onChange={(e) => onFormUpdate("message", e.target.value)}
